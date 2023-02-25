74f959db041af9afb909f9f961b728aa401bb400
Asritha Mulugoju <76226345+asrithaMulugoju@users.noreply.github.com>
2023-02-25 11:14:46 +0530

    teamA.py

---
+++ teamA.py
@@ -0,0 +1,6 @@
+num_of_toys = int(input())
+total_amount = int(input())
+amount_left = int(input())
+each_toy_price = total_amount // num_of_toys
+remaining_amount = total_amount + amount_left
+print("Cost of each toy is", each_toy_price , "Amount he had is" , remaining_amount)
